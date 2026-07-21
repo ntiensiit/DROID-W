bb1bd81c3f53e35b57518a0fded8c8320eaf3e25
ntiensiit <ntiensi.it@gmail.com>
2026-07-21 23:58:47 +0700

    Fix conditional check for Miniconda installation in setup script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ set -e
 
 apt-get update && apt-get install -y git wget build-essential unzip
 
-if [ ! -d "/opt/miniconda" ]; then
+if ! command -v conda &> /dev/null; then
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
     bash miniconda.sh -b -p /opt/miniconda
     rm miniconda.sh
